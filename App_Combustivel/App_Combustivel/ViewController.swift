e3877b72d9160efc3ad8b7e9e8b7ca7b9cd0e3c1
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-04-19 21:49:21 -0300

    Outlets

--- App_Combustivel/App_Combustivel/ViewController.swift
+++ App_Combustivel/App_Combustivel/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tfAlcool: UITextField!
+    @IBOutlet weak var tfGasoline: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func calculatePrice(_ sender: Any) {
+    }
+    
 
 }
 
